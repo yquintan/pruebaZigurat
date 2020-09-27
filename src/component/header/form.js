4d888b061a931d69617903bccfa4e0db328750a7
yquin <yquin@gmail.com>
2020-09-27 19:45:44 +0200

    se modifico

--- src/component/header/form.js
+++ src/component/header/form.js
@@ -1,6 +1,8 @@
 import React, { Fragment } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./form.scss";
+import { FormControl, FormLabel } from 'react-bootstrap';
+import { Form, FormGroup,Button } from 'reactstrap';
 
 function form() {
 
@@ -22,68 +24,66 @@ function form() {
      return(
          <Fragment>
              <div className="col-md-6 cont">
-                <form className="col-md-12 col-sm-11  mx-auto my-10">
+                <Form className="col-md-12 col-sm-11  mx-auto my-10">
 
                      <div className="row">
-                         <div className="form-group col-md-6">
-                             <label className="label_as" for="name">Name</label>
-                             <input type="text" class="form-control" id="name" required/>
-                         </div>
+                         <FormGroup className="form-group col-md-6">
+                             <FormLabel className="label_as" for="name">Name</FormLabel>
+                             <FormControl type="text" class="form-control" id="name" required/>
+                         </FormGroup>
                           
-                         <div className="form-group col-md-6">
-                                <label className="label_as" for="lastname">Apellido</label>
-                                <input type="text" className="form-control" id="lastname" required/>
-                         </div>                           
+                         <FormGroup className="form-group col-md-6">
+                                <FormLabel className="label_as" for="lastname">Apellido</FormLabel>
+                                <FormControl type="text" className="form-control" id="lastname" required/>
+                         </FormGroup>                           
                       </div>
 
                         <div className="row">
-                            <div className="form-group col-md-6">
-                                <label className="label_as" for="selectPrefijo">Prefijo</label>
-                                <select className="form-control" id="selectPrefijo">
+                            <FormGroup className="form-group col-md-6">
+                                <FormLabel className="label_as" for="selectPrefijo">Prefijo</ FormLabel>
+                                <FormControl as="select" className="form-control" id="selectPrefijo">
 
                                     {dataPrefijo.map((item)=>(
                                             <option >{item.prefijo}</option>
                                             )
                                         )
-                                        }
-                                   
-                                </select>
-                            </div>
+                                        }                                   
+                                </FormControl>
+                            </FormGroup>
 
-                            <div className="form-group col-md-6">
-                                <label className="label_as" for="telefono">Teléfono</label>
-                                <input type="text" className="form-control" id="telefono" required/>
-                            </div>
+                            <FormGroup className="form-group col-md-6">
+                                <FormLabel className="label_as" for="telefono">Teléfono</FormLabel>
+                                <FormControl type="text" className="form-control" id="telefono" required/>
+                            </FormGroup>
                         </div>
 
                         <div className="row">
-                            <div className="form-group col-md-6">
-                                <label className="label_as" for="pais">País</label>
-                                <select className="form-control" id="pais">
+                            <FormGroup className="form-group col-md-6">
+                                <FormLabel className="label_as" for="pais">País</FormLabel>
+                                <FormControl as="select" className="form-control" id="pais">
                                     {dataCity.map(
                                          (item)=>(
                                          <option> {item.city}</option>
                                          )
                                      )
-                                     }
+                                     }                                  
                                     
-                                    
-                                </select>
-                            </div>
-                            <div className="form-group col-md-6">
-                                <label className="label_as" for="email">Email</label>
-                                <input type="email" className="form-control" id="email" required/>
-                            </div>
+                                </FormControl>
+                            </FormGroup>
+                            <FormGroup className="form-group col-md-6">
+                                <FormLabel className="label_as" for="email">Email</FormLabel>
+                                <FormControl type="email" className="form-control" id="email" required/>
+                            </FormGroup>
                         </div>
 
-                        <div className="form-group form-check col-md-12 my-4">
+                        <FormGroup className="form-group form-check col-md-12 my-4">
                             <input type="checkbox" className="form-check-input" id="check" required/>
-                            <label className="form-check-label" for="check"></label>
-                            <small className="form-text d-inline">Al hacer clic en el botón, usted acepta los términos de uso, política de privacidad,política de cookies y aviso legal.</small>
-                        </div>
-                    
+                            <label className="form-check-label" for="check">
+                            <small className="form-text d-inline">Al hacer clic en el botón, usted acepta los términos de uso, política de privacidad,política de cookies y aviso legal.</small></label>                             
+                        </FormGroup>
+                                            
                         <button type="submit" className="btn btn-outline-dark" id="mui_btn_color">Más información</button> 
-                </form>
+                </Form>
                 </div>
            
          </Fragment>
